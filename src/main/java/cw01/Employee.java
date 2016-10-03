214205701aecf56c24bc7f7c05b56795602e41f0
mankunianeth <deyneko55@gmail.com>
2016-10-03 11:41:01 +0300

    Add cw01 examples.

---
+++ src/main/java/cw01/Employee.java
@@ -0,0 +1,9 @@
+package cw01;
+
+/**
+ * Created by evgeniy on 03/10/16.
+ */
+public class Employee {
+    String name; // null
+    int salary; // 0
+}
--- src/main/java/cw01/Main.java
+++ src/main/java/cw01/Main.java
@@ -1,11 +1,39 @@
 package cw01;
 
-import org.junit.Test;
-
 public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        System.out.println(employee1.name);
+        System.out.println(employee1.salary);
 
-    @Test
-    public void test() {
+        employee1.salary = 10000;
+        employee1.name = "Boby";
 
+        System.out.println(employee1.name);
+        System.out.println(employee1.salary);
+
+        Employee employee2 = new Employee();
+        employee2.salary = 4000;
+        employee2.name = "Vily";
+
+        Employee employee3 = new Employee();
+        employee3.salary = 3000;
+        employee3.name = "Vova";
+
+        Employee[] employees = {employee1, employee2, employee3};
+
+        System.out.println(getMaxSalaryEmployee(employees).name);
+    }
+
+    static Employee getMaxSalaryEmployee(Employee[] employees) {
+        int maxSalary = employees[0].salary;
+        Employee maxSalaryEmployee = employees[0];
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].salary > maxSalary) {
+                maxSalary = employees[i].salary;
+                maxSalaryEmployee = employees[i];
+            }
+        }
+        return maxSalaryEmployee;
     }
 }
