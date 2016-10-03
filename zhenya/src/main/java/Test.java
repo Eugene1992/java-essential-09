261625b71a174f0356e46a6f000cb02f301ef893
mankunianeth <deyneko55@gmail.com>
2016-10-03 10:33:41 +0300

    Second test commit.

--- zhenya/src/main/java/Test.java
+++ zhenya/src/main/java/Test.java
@@ -2,4 +2,9 @@
  * Created by evgeniy on 03/10/16.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
 }
